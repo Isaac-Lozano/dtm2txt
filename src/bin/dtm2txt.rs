04eca84331c462f6bb322a7fb18e2ce31d16a204
Isaac Lozano <Isaac-Lozano@users.noreply.github.com>
2018-08-19 14:48:02 -0700

    dtm2txt: no more unwraps

--- src/bin/dtm2txt.rs
+++ src/bin/dtm2txt.rs
@@ -1,14 +1,45 @@
 extern crate dtm2txt;
 
 use std::env;
+use std::error::Error;
 use std::fs::File;
 use std::io::{BufReader, BufWriter};
 use std::path::{PathBuf};
+use std::process;
 
 use dtm2txt::encoder::text_encoder::TextEncoder;
 use dtm2txt::encoder::dtm_encoder::DtmEncoder;
 use dtm2txt::decoder::text_decoder::TextDecoder;
 use dtm2txt::decoder::dtm_decoder::DtmDecoder;
+
+trait UnwrapOrBarfExt<T> {
+    fn unwrap_or_barf(self, err_str: &str) -> T;
+}
+
+impl<T, E> UnwrapOrBarfExt<T> for Result<T, E>
+    where E: Error
+{
+    fn unwrap_or_barf(self, err_desc: &str) -> T {
+        self.unwrap_or_else(|err| {
+            let err_string = format!("{}: {}", err_desc, err);
+            barf(&err_string);
+        })
+    }
+}
+
+impl<T> UnwrapOrBarfExt<T> for Option<T> {
+    fn unwrap_or_barf(self, err_desc: &str) -> T {
+        self.unwrap_or_else(|| {
+            let err_string = format!("{}", err_desc);
+            barf(&err_string);
+        })
+    }
+}
+
+fn barf(message: &str) -> ! {
+    println!("Error: {}", message);
+    process::exit(1);
+}
 
 fn main() {
     let mut args = env::args().skip(1);
@@ -22,29 +53,29 @@ fn main() {
     };
 
     let filename: PathBuf = filename_string.into();
-    let file = BufReader::new(File::open(&filename).unwrap());
+    let file = BufReader::new(File::open(&filename).unwrap_or_barf("Could not file"));
 
-    match filename.extension().unwrap().to_str().unwrap() {
+    match filename.extension().unwrap_or_barf("Filename has no extension").to_str().unwrap_or_barf("Error processing filename") {
         "dtm" => {
             let decoder = DtmDecoder::new(file);
-            let dtm_bin = decoder.decode().unwrap();
+            let dtm_bin = decoder.decode().unwrap_or_barf("Could not make dtm decoder");
 
             let output_filename = filename.with_extension("txt");
-            let output_file = BufWriter::new(File::create(output_filename).unwrap());
+            let output_file = BufWriter::new(File::create(output_filename).unwrap_or_barf("Could not create file"));
 
             let encoder = TextEncoder::new(output_file);
-            encoder.encode(&dtm_bin).unwrap();
+            encoder.encode(&dtm_bin).unwrap_or_barf("Could not encode dtm");
         }
         "txt" => {
             let decoder = TextDecoder::new(file);
-            let dtm_txt = decoder.decode().unwrap();
+            let dtm_txt = decoder.decode().unwrap_or_barf("Could not make text decoder");
 
             let output_filename = filename.with_extension("dtm");
-            let output_file = BufWriter::new(File::create(output_filename).unwrap());
+            let output_file = BufWriter::new(File::create(output_filename).unwrap_or_barf("Could not create file"));
 
             let encoder = DtmEncoder::new(output_file);
-            encoder.encode(&dtm_txt).unwrap();
+            encoder.encode(&dtm_txt).unwrap_or_barf("Could not encode dtm");
         }
-        _ => panic!("File must be a txt or a dtm."),
+        _ => barf("File must be a txt or a dtm."),
     }
 }
